7e6fab08ae4ac6c51ab4ff5fb8a48f3575649f9e
janekjan <jas.piskorski@spoko.pl>
2017-02-02 23:18:19 +0100

    new get-weight

--- punish.scm
+++ punish.scm
@@ -1,24 +1,56 @@
 ;;;Punish commands definition
 ;;; PETROVICH
-
+#lang racket
+(require racket/format)
+(define ns (make-base-namespace))
 
 (define weights-path "petrovich/config/weights/")
 (define default-weights-path "petrovich/config/default-weights/")
 (define history-path "petrovich/config/history")
 
-(define (get-weights command)
-  (load (string-append weights-path command)))
-;(cadar (get-weights "k3"))
 
+;;The global, fallback, default weight for commands
+(define (set-global-weight comm w)
+  (let ((p (open-output-file (string-append default-weights-path comm) #:exists 'replace)))
+    (display w p)
+    (newline p)
+    (close-output-port p)))
+
+(define (get-global-weight comm)
+  (parameterize ((current-namespace ns))
+    (load (string-append default-weights-path (~a comm)))))
+
+;;the proper weights
+(define (get-weights command)
+  (parameterize ((current-namespace ns))
+               (load (string-append weights-path command))))
+;;finding last one doesnt work!!!
+(define (get-weight command what)
+      (letrec ((find-the-weight (lambda (weights)
+                                  (if (or (equal? weights '()) (void? weights))
+                                      (get-global-weight command)
+                                      (if (equal? (cdr weights) '())
+                                          (get-global-weight command)
+                                          ;fallback weight previous line, proper one below:
+                                          (if (equal? (caar weights) what)
+                                              (cadar weights)
+                                              (find-the-weight (cdr weights))))))))
+        (find-the-weight (get-weights command))))
+                
+                
 ;;These lambdas get command history in diferrent, convenient ways
 (define (get-history)
   (let ((p (open-input-file history-path)))
-  (let f ((x (read p)))
-    (if (eof-object? x)
-        (begin
-          (close-input-port p)
-          '())
-        (cons x (f (read p)))))))
+    (let f ((x (read p)))
+      (if (eof-object? x)
+          (begin
+            (close-input-port p)
+            '())
+          (cons x (f (read p)))))))
+
+(define (current-command)
+  (let ((h (reverse (get-history))))
+    (~a (car h))))
 
 (define (get-last-3)
   (let ((h (reverse (get-history))))
@@ -34,7 +66,7 @@
 
 ;;Add to history - move elswhere
 (define (add-to-history comm)
-  (let ((h (open-output-file history-path 'append)))
+  (let ((h (open-output-file history-path #:exists 'append)))
     (display comm h)
     (newline h)
     (close-output-port h)))
@@ -70,24 +102,30 @@
                                     (write-to-weight comm (find-insert weights '()))))
                                  (else (find-and-write (cdr li)))))))
       (find-and-write weights))))
-
-;;The global, fallback, default weight for commands
-(define (set-global-weight comm w)
-  (let ((p (open-output-file (string-append default-weights-path comm) 'replace)))
-    (display w p)
-    (newline p)
-    (close-output-port p)))
-
-(define (get-global-weight comm)
-  (load (string-append default-weights-path comm)))
   
 ;;PUNISH and REWARD!!! :-) :D
-;(define (punish)
+(define (punish)
+  (let ((c (current-command))
+        (l1 (get-last))
+        (l2 (get-last-2))
+        (l3 (get-last-3)))
+    (set-global-weight c (- (get-global-weight c) 1))
+    ;(set-weight c l3 (- (get-weight
+    ))
   
 ;(set-global-weight "k53" 40)
 ;(set-weight "k3" '(k3 k5 k8) 15)
 ;(get-global-weight "k53")
+;(add-to-history "ham2")
+
+;(punish)
+(get-global-weight "ham2")
 (get-history)
 (get-last-3)
 (get-last-2)
 (get-last)
+(current-command)
+
+(get-weights "k3")
+(get-global-weight "k3")
+(get-weight "k3" '(k1 k2))
